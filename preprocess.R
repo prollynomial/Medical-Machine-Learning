4f436b2161d4667ee3e4a29719032b32da64f3a4
Adam Carruthers <adam@bitjutsu.ca>
2015-11-29 22:57:56 -0500

    Add data preprocessing script, initial baseline learner

---
+++ preprocess.R
@@ -0,0 +1,35 @@
+data <- read.csv('dataset_diabetes/diabetic_data.csv', stringsAsFactors=F)
+
+# ??? Change from 3-class classification to binary classification ???
+target.column <- 'readmitted'
+non.predictive.columns <- c('encounter_id', 'patient_nbr')
+
+# These columns have > 50% missing data
+dropped.columns <- c('weight', 'payer_code', 'medical_specialty')
+
+data <- data[!(names(data) %in% non.predictive.columns)]
+data <- data[!(names(data) %in% dropped.columns)]
+
+# Replace factors with integer levels
+feature.names <- names(data)
+for (f in feature.names) {
+	if (class(data[[f]]) == "character") {
+		levels <- unique(data[[f]])
+		data[[f]] <- as.integer(factor(data[[f]], levels=levels))
+	}
+}
+
+# Shuffle
+num.examples <- nrow(data)
+data <- data[sample(num.examples),]
+
+# Set train:test split of 80:20
+split <- as.integer(0.8 * num.examples)
+train <- data[1:split,]
+test <- data[split:num.examples,]
+
+# Output {train,test}_{x,y}.csv
+write.csv(train[names(train) != target.column], 'train_x.csv', row.names=F)
+write.csv(train[names(train) == target.column], 'train_y.csv', row.names=F)
+write.csv(test[names(test) != target.column], 'test_x.csv', row.names=F)
+write.csv(test[names(test) == target.column], 'test_y.csv', row.names=F)
---
+++ train_randomforest.py
@@ -0,0 +1,67 @@
+from sklearn.ensemble import RandomForestClassifier as Classifier
+from sklearn.metrics import zero_one_loss as loss
+import pandas as pd
+import numpy as np
+
+# Seed numpy rng
+np.random.seed(123)
+
+train_x = pd.read_csv('train_x.csv')
+train_y = pd.read_csv('train_y.csv')
+
+split = int(0.8 * len(train_x))
+train_x, valid_x = train_x[:split], train_x[split:]
+train_y, valid_y = train_y[:split], train_y[split:]
+
+# Convert train_y to a 1d array to silence sklearn conversion warnings
+train_y = train_y.as_matrix().reshape((train_y.shape[0],))
+
+test_x = pd.read_csv('test_x.csv')
+test_y = pd.read_csv('test_y.csv')
+
+# Using CV, discovered n=525 to be best (rng is seeded, results will not change)
+n_estimators = [ 525 ]
+best_error = float('Inf')
+best_model = None
+best_n = None
+
+for n in n_estimators:
+	clf = Classifier(n_estimators=n, criterion='entropy', n_jobs=-1, random_state=123)
+	clf.fit(train_x, train_y)
+
+	predictions = clf.predict(valid_x)
+	error = loss(valid_y, predictions)
+	print "Validation error: " + str(error)
+
+	if error < best_error:
+		best_error = error
+		best_model = clf
+		best_n = n
+
+print "Best n: " + str(best_n)
+
+predictions = best_model.predict(test_x)
+test_error = loss(test_y, predictions)
+print "Test error: " + str(test_error)
+
+feature_names = train_x.columns.values.tolist()
+
+features_errors = []
+for feature in feature_names:
+	errors = []
+	# Get an average over 5 shuffles to smooth out results
+	for i in range(5):
+		test_x_modified = test_x.copy()
+		np.random.shuffle(test_x_modified[feature])
+		predictions = best_model.predict(test_x_modified)
+		error = loss(test_y, predictions)
+		errors.append(error)
+
+	avg_shuffled_error = sum(errors) / 5.
+	features_errors.append((feature, avg_shuffled_error - test_error))
+
+features_errors = sorted(features_errors, key=lambda x: x[1], reverse=True)
+
+print "Feature's error differences (error_shuffled - test_error):"
+for idx, (feature, error_diff) in enumerate(features_errors):
+	print str(idx + 1) + ") " + feature + ": " + str(error_diff)
